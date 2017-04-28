d228dfef946f94256500886ff4d6549fe570b036
Dmitriy Mikhaylik <sblazze@gmail.com>
2017-04-28 12:17:28 +0300

    Первая страница. Hello world! :)

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -122,4 +122,11 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+
+    public function actionHello($message = "Hello world!")
+    {
+        return $this->render('hello', ['message' => $message]); // альтернатива ['message' => $message]     -    compact('message')
+    }
+
 }
